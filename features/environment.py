c765a26ad78a30a70fb5f51ea71bfc7ca78a551a
hrodriguez <holger.rodriguez@roseng.ch>
2023-07-08 17:18:57 +0200

    we only support chrome

--- features/environment.py
+++ features/environment.py
@@ -9,12 +9,7 @@ def before_scenario(context,driver):
 
     browser_name = ConfigReader.read_configuration("basic info","browser")
 
-    if browser_name.__eq__("chrome"):
-        context.driver = webdriver.Chrome()
-    elif browser_name.__eq__("firefox"):
-        context.driver = webdriver.Firefox()
-    elif browser_name.__eq__("edge"):
-        context.driver = webdriver.Edge()
+    context.driver = webdriver.Chrome()
 
     context.driver.maximize_window()
     context.driver.get(ConfigReader.read_configuration("basic info","url"))
